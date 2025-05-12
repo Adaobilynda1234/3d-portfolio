31f8e5bc325fb2a0c330ddf165615bb16071112c
Adaobilynda1234 <adaobilynda1234@gmail.com>
2025-05-12 22:50:51 +0100

    updated css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,9 +18,8 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915EFF]">Adaobi </span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="sm:block hidden" />
-            interfaces and web applications
+          <p className={`${styles.heroSubText} mt-2 text-white-100 z-10`}>
+            A frontend Developer
           </p>
         </div>
       </div>
