df8af9e52e07bbdddc41c9bacfe0d194464c958f
Adaobilynda1234 <adaobilynda1234@gmail.com>
2025-05-17 00:41:22 +0100

    fixed hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915EFF]">Adaobi </span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100 z-10`}>
+          <p className={`${styles.heroSubText} mt-2 text-white-100 `}>
             A frontend Developer
           </p>
         </div>
